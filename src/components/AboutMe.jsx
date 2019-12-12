3880c30453833e35ff454931731a81245158cd69
Ian Rappaport <ianrappaport@Ians-MacBook-Pro-2.local>
2019-12-12 16:05:21 -0500

    home button underline annoying

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,6 +5,18 @@ export default function AboutMe() {
   return (
     <div id="about-me">
       This is About Me
+      <div>
+        <h1>Skills</h1>
+        <p>Javascript</p>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>React</p>
+        <p>Ruby</p>
+        <p>Ruby on Rails</p>
+        <p>Express</p>
+        <p>SQL</p>
+        <p>Django</p>
+      </div>
     </div>
   )
 }
