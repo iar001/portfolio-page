00677f07cc4fe729ca588d4e532bc5fcbd03fd3c
Ian Rappaport <ianrappaport@Ians-MacBook-Pro-2.local>
2019-12-12 14:52:13 -0500

    react up and running

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Route, Link, withRouter } from 'react-router-dom';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App" >
+        Hello
+      </div>
+    );
+  }
+
+}
+
+export default App;
